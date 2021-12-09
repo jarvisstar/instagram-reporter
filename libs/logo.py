75bc9d296b03e946cb210bdb17d60ec255b4723b
rAj <52195362+rajbhx@users.noreply.github.com>
2021-12-09 18:46:04 +0530

    Update logo.py

--- libs/logo.py
+++ libs/logo.py
@@ -4,22 +4,25 @@ from colorama import Fore, Back, Style
 from random import choice
 
 logo = """
-╭━━╮╱╱╱╱╱╭╮╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╱╭╮
-╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃╭━╮┃╱╱╱╱╱╱╱╱╱╭╯╰╮
-╱┃┃╭━╮╭━┻╮╭╋━━┫╰━╯┣━━┳━━┳━━┳┻╮╭╋━━┳━╮
-╱┃┃┃╭╮┫━━┫┃┃╭╮┃╭╮╭┫┃━┫╭╮┃╭╮┃╭┫┃┃┃━┫╭╯
-╭┫┣┫┃┃┣━━┃╰┫╭╮┃┃┃╰┫┃━┫╰╯┃╰╯┃┃┃╰┫┃━┫┃
-╰━━┻╯╰┻━━┻━┻╯╰┻╯╰━┻━━┫╭━┻━━┻╯╰━┻━━┻╯
-╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
-╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯"""
+                                                     /$$                        
+                                                    | $$                        
+  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$ 
+ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/   /$$__  $$ /$$__  $$
+| $$  \__/| $$$$$$$$| $$  \ $$| $$  \ $$| $$  \__/  | $$    | $$$$$$$$| $$  \__/
+| $$      | $$_____/| $$  | $$| $$  | $$| $$        | $$ /$$| $$_____/| $$      
+| $$      |  $$$$$$$| $$$$$$$/|  $$$$$$/| $$        |  $$$$/|  $$$$$$$| $$      
+|__/       \_______/| $$____/  \______/ |__/         \___/   \_______/|__/      
+                    | $$                                                        
+                    | $$                                                        
+                    |__/ """
 
 urls = [
-    "GitHub - https://github.com/muneebwanee",
-    "Instagram - https://instagram.com/muneebwanee",
-    "Facebook - https://fb.com/muneebwane",
-    "Twitter - https://twitter.com/muneebwanee",
-    "InstaReporter Tool - https://github.com/muneebwanee/InstaReporter",
-    "Gmail - mailto:muneebwanee@gmail.com"
+    "GitHub - https://github.com/rajbhx",
+    "Instagram - https://instagram.com/rajbhx",
+    "Facebook - https://fb.com/rajbhx",
+    "Twitter - https://twitter.com/rajbhx",
+    "Instagram-Reporter Tool - https://github.com/jarvisstar/instagram-reporter",
+    "Gmail - mailto:rajbhx@gmail.com"
     ]
 
 def print_logo():
